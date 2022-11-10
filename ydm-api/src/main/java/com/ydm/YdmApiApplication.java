42840260a733330942d1f6389297401aabe7a2eb
Yan Maia <yandamasceno01@gmail.com>
2022-11-10 00:59:18 -0300

    criacao de projeto em spring boot

---
+++ ydm-api/src/main/java/com/ydm/YdmApiApplication.java
@@ -0,0 +1,13 @@
+package com.ydm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YdmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YdmApiApplication.class, args);
+	}
+
+}
---
+++ ydm-api/src/test/java/com/ydm/YdmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YdmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
